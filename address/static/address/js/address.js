6886aa345f60b56d679ed6c0e42ace489d41815a
Micah Lyle <micahlyle@gmail.com>
2017-06-15 00:56:32 -0700

    Tiny style and possibly other changes

--- address/static/address/js/address.js
+++ address/static/address/js/address.js
@@ -7,13 +7,13 @@ $(function() {
             details: cmps,
             detailsAttribute: 'data-geo'
         }).change(function() {
-	    if (self.val() != fmtd.val()) {
-			var cmp_names = ['country', 'country_code', 'locality', 'postal_code',
-							 'route', 'street_number', 'state', 'state_code',
-							 'formatted', 'latitude', 'longitude'];
-			for(var ii = 0; ii < cmp_names.length; ++ii)
-				$('input[name="' + self.attr('name') + '_' + cmp_names[ii] + '"]').val('');
-	    }
-	});
+			if (self.val() != fmtd.val()) {
+				var cmp_names = ['country', 'country_code', 'locality', 'postal_code',
+								'route', 'street_number', 'state', 'state_code',
+								'formatted', 'latitude', 'longitude'];
+				for(var ii = 0; ii < cmp_names.length; ++ii)
+					$('input[name="' + self.attr('name') + '_' + cmp_names[ii] + '"]').val('');
+			}
+		});
     });
 });
