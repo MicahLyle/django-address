0cf57252ac70379ba17c55d1c2e56bd8c9707456
Micah Lyle <micahlyle@gmail.com>
2017-12-31 00:08:11 -0800

    Fixed more model stuff for django 2.0

--- address/models.py
+++ address/models.py
@@ -196,7 +196,7 @@ class State(models.Model):
 class Locality(models.Model):
     name = models.CharField(max_length=165, blank=True)
     postal_code = models.CharField(max_length=10, blank=True)
-    state = models.ForeignKey(State, related_name='localities')
+    state = models.ForeignKey(State, related_name='localities', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'Localities'
@@ -224,7 +224,7 @@ class Locality(models.Model):
 class Address(models.Model):
     street_number = models.CharField(max_length=20, blank=True)
     route = models.CharField(max_length=100, blank=True)
-    locality = models.ForeignKey(Locality, related_name='addresses', blank=True, null=True)
+    locality = models.ForeignKey(Locality, related_name='addresses', blank=True, null=True, on_delete=models.CASCADE)
     raw = models.CharField(max_length=200)
     formatted = models.CharField(max_length=200, blank=True)
     latitude = models.FloatField(blank=True, null=True)
